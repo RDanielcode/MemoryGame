9b2612c5053f4ae17b53cbd9de2adb69b2ece317
Danielcode <danielalejndro@gmail.com>
2022-06-10 14:56:00 -0400

    improving interface

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const blue = document.getElementById('blue');
 const green = document.getElementById('green');
 const yellow = document.getElementById('yellow');
 const btnStart = document.getElementById('btnStart');
+const levelBoard = document.getElementById('level')
 const LAST_LEVEL = 10;
 
 
@@ -10,11 +11,13 @@ class Game{
     constructor(){
         this.starting();
         this.sequenceGen();
+        this.levelOne();
         this.nextLevel();
     }
-
+    
     starting(){
         this.chooseColor = this.chooseColor.bind(this)
+        // this.levelBoard = this.levelBoard.bind(this)
         this.nextLevel = this.nextLevel.bind(this)
         this.toogleBtnStart()
         this.toogleDivStart()
@@ -26,33 +29,45 @@ class Game{
             yellow
         }
     }
-
+    
+    levelOne(){
+        levelBoard.classList.add('modal')
+        levelBoard.innerHTML = `        <div class="box-inside">
+        <h1 class="level">You are in level 1</h1>
+        <p>Pick the pokemon</p>
+    </div>`
+        setTimeout(()=> {
+            levelBoard.innerHTML = ''
+            levelBoard.classList.remove('modal')
+        }, 3000)
+    }
+    
     toogleDivStart(){
         if(!red.classList.contains('red')){
             red.classList.add('red')
         } else{
             red.classList.remove('red')
         }
-
+        
         if(!blue.classList.contains('blue')){
             blue.classList.add('blue')
         } else{
             blue.classList.remove('blue')
         }
-
+        
         if(!green.classList.contains('green')){
             green.classList.add('green')
         } else{
             green.classList.remove('green')
         }
-
+        
         if(!yellow.classList.contains('yellow')){
             yellow.classList.add('yellow')
         } else{
             yellow.classList.remove('yellow')
         }
     }
-
+    
     toogleBtnStart(){
         if(btnStart.classList.contains('hide')){
             btnStart.classList.remove('hide')
@@ -60,12 +75,12 @@ class Game{
             btnStart.classList.add('hide')
         }
     }
-
+    
     sequenceGen(){
         this.sequence = new Array(LAST_LEVEL).fill(0).map(n => Math.floor(Math.random() * 4));
         console.log(this.sequence)
     }
-
+    
     nextLevel(){
         this.sublevel = 0;
         this.lightSequence()
@@ -103,7 +118,7 @@ class Game{
             const color = this.transformNumber(this.sequence[i]);
             setTimeout(() =>{
                 this.lightColor(color);
-            }, 1000 * i)  
+            }, 4000+1000 * i)  
         }
     }
 
@@ -140,7 +155,8 @@ class Game{
         if (numberColor === this.sequence[this.sublevel]){
             this.sublevel++;
             if(this.sublevel === this.level){
-                this.level++;
+                const newLevel = this.level++;
+                this.newLevelAdvanced(newLevel)
                 this.deleteClickEvent();
                 if(this.level === LAST_LEVEL + 1){
                     this.gameWon()
@@ -151,6 +167,20 @@ class Game{
         }else {
             this.gameLost()
         }
+    }
+
+    newLevelAdvanced(level){
+        setTimeout(()=> {
+            levelBoard.classList.add('modal')
+            levelBoard.innerHTML = `        <div class="box-inside">
+            <h1 class="level">You passed to level ${level + 1}</h1>
+            <p>Pick the pokemon and REMEMBER the sequence</p>
+        </div>`
+        }, 1000)
+        setTimeout(()=> {
+            levelBoard.innerHTML = ''
+            levelBoard.classList.remove('modal')
+        }, 4000)
     }
 
     gameLost(){
